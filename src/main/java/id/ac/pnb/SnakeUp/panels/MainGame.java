6ad9cb857a9e9e2be1931da00ac5a3340b19292d
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-17 19:41:26 +0800

    feat(panels): add game event and game services

--- src/main/java/id/ac/pnb/SnakeUp/panels/MainGame.java
+++ src/main/java/id/ac/pnb/SnakeUp/panels/MainGame.java
@@ -1,19 +1,44 @@
 package id.ac.pnb.SnakeUp.panels;
 
+import id.ac.pnb.SnakeUp.components.GameEvent;
 import id.ac.pnb.SnakeUp.components.GamePanel;
-import id.ac.pnb.SnakeUp.models.Board;
+import id.ac.pnb.SnakeUp.models.*;
+import id.ac.pnb.SnakeUp.services.GameService;
+import id.ac.pnb.SnakeUp.services.game.ScoreBoard;
+import id.ac.pnb.SnakeUp.services.game.SnakeLadderGame;
+import id.ac.pnb.SnakeUp.services.game.SnakeLadderGameEvent;
 
 import java.awt.*;
 
 public class MainGame extends GamePanel {
 
+  private Board board;
+  private Player player;
+  private Dice dice;
+  private Snake snake;
+  private Ladder ladder;
+
+  private GameService gameService;
+  private GameService scoreService;
+
+  private GameEvent gameEvent;
+
   public MainGame() {
+    _initialize();
+    addMouseListener(gameEvent);
   }
 
   public final void paintComponent(Graphics g) {
     super.paintComponent(g);
+    gameService.start(g);
+  }
 
-    var board = new Board(this.w, this.h);
-    board.create(g);
+  private void _initialize() {
+    board = new Board();
+    player = new Player();
+    dice = new Dice();
+    scoreService = new ScoreBoard();
+    gameService = new SnakeLadderGame(board, player, dice, scoreService);
+    gameEvent = new SnakeLadderGameEvent(gameService);
   }
 }
