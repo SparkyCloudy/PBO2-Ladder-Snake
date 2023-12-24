41a1cc29814c79905e3d2279d9238a0d28304f91
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-24 21:39:23 +0800

    feat(services): add roll dice event

--- src/main/java/id/ac/pnb/SnakeUp/services/game/SnakeLadderGameEvent.java
+++ src/main/java/id/ac/pnb/SnakeUp/services/game/SnakeLadderGameEvent.java
@@ -1,6 +1,8 @@
 package id.ac.pnb.SnakeUp.services.game;
 
 import id.ac.pnb.SnakeUp.components.GameEvent;
+import id.ac.pnb.SnakeUp.components.GamePanel;
+import id.ac.pnb.SnakeUp.models.PlayerImpl;
 import id.ac.pnb.SnakeUp.services.GameService;
 
 import java.awt.event.MouseEvent;
@@ -8,16 +10,53 @@ import java.awt.event.MouseEvent;
 public class SnakeLadderGameEvent extends GameEvent {
 
   private final SnakeLadderGame GAME_SERVICE;
+  private final GamePanel PANEL;
 
-  public SnakeLadderGameEvent(GameService gameService) {
+  public SnakeLadderGameEvent(GameService gameService, GamePanel panel) {
     super();
+    this.PANEL = panel;
     this.GAME_SERVICE = (SnakeLadderGame) gameService;
   }
 
   @Override
   public void mousePressed(MouseEvent e) {
     super.mousePressed(e);
-    onTileClick(e);
+    if (GAME_SERVICE.isNextTurn()) {
+      onTileClick(e);
+    } else {
+      onDiceRolled();
+    }
+  }
+
+  private void onDiceRolled() {
+    var dice = GAME_SERVICE.getDice();
+    var tiles = GAME_SERVICE.getBoard().getTiles();
+    var playerTurn = GAME_SERVICE.getPlayerTurn();
+    var player = (PlayerImpl) PlayerManager.getPlayer(playerTurn);
+
+    dice.randomizeValue();
+    player.addDiceValue(dice.getValue() + 1);
+
+    var currentDiceValue = player.getCurrentDiceValue();
+    if (currentDiceValue > tiles.size()-1) {
+      var remain = currentDiceValue - tiles.size();
+      player.setCurrentDiceValue(tiles.size()-2 - remain);
+      currentDiceValue = player.getCurrentDiceValue();
+    }
+
+    var currentPos = tiles.get(currentDiceValue).getPosition();
+    PlayerManager.getPlayer(playerTurn).getPosition()
+            .setLocation(currentPos);
+
+    dice.create(PANEL.getGraphics());
+
+    if (PlayerManager.getPlayer(playerTurn).getPosition()
+        .equals(GAME_SERVICE.getBoard().getEndPosition())) {
+      System.out.println("Player " + playerTurn + " is winning");
+      GAME_SERVICE.stop();
+    }
+
+    GAME_SERVICE.setIsNextTurn(true);
   }
 
   private void onTileClick(MouseEvent e) {
