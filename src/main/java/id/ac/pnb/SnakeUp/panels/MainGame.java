309714d518ec648c96c352b71a69358272f4ccdd
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-24 19:42:34 +0800

    feat(panels): add singleton services mechanism with list

--- src/main/java/id/ac/pnb/SnakeUp/panels/MainGame.java
+++ src/main/java/id/ac/pnb/SnakeUp/panels/MainGame.java
@@ -1,44 +1,43 @@
 package id.ac.pnb.SnakeUp.panels;
 
-import id.ac.pnb.SnakeUp.components.GameEvent;
 import id.ac.pnb.SnakeUp.components.GamePanel;
-import id.ac.pnb.SnakeUp.models.*;
 import id.ac.pnb.SnakeUp.services.GameService;
 import id.ac.pnb.SnakeUp.services.game.ScoreBoard;
 import id.ac.pnb.SnakeUp.services.game.SnakeLadderGame;
 import id.ac.pnb.SnakeUp.services.game.SnakeLadderGameEvent;
 
-import java.awt.*;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainGame extends GamePanel {
 
-  private Board board;
-  private Player player;
-  private Dice dice;
-  private Snake snake;
-  private Ladder ladder;
+  private final GameService gameService;
+  private final GameService scoreService;
 
-  private GameService gameService;
-  private GameService scoreService;
+  private final List<GameService> services = new ArrayList<>();
 
-  private GameEvent gameEvent;
-
-  public MainGame() {
+  public MainGame(int playerCount) {
+    scoreService = new ScoreBoard();
+    gameService = new SnakeLadderGame(playerCount, scoreService);
     _initialize();
-    addMouseListener(gameEvent);
   }
 
   public final void paintComponent(Graphics g) {
     super.paintComponent(g);
-    gameService.start(g);
+    scoreService.draw(g);
+    gameService.draw(g);
   }
 
   private void _initialize() {
-    board = new Board();
-    player = new Player();
-    dice = new Dice();
-    scoreService = new ScoreBoard();
-    gameService = new SnakeLadderGame(board, player, dice, scoreService);
-    gameEvent = new SnakeLadderGameEvent(gameService);
+    services.add(gameService);
+    services.add(scoreService);
+
+    for (var service : services) {
+      service.start();
+    }
+
+    var gameEvent = new SnakeLadderGameEvent(gameService, this);
+    addMouseListener(gameEvent);
   }
 }
