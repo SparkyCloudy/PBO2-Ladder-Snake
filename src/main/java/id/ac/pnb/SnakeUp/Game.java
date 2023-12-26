ea87e259cb7553feda1982f5e1b05a956e2017e8
Prajna Paramahansa <satriagama1@gmail.com>
2023-12-26 15:05:32 +0800

    feat: add new game loop logic

--- src/main/java/id/ac/pnb/SnakeUp/Game.java
+++ src/main/java/id/ac/pnb/SnakeUp/Game.java
@@ -4,6 +4,7 @@ import id.ac.pnb.SnakeUp.components.GamePanel;
 import id.ac.pnb.SnakeUp.components.GameWindow;
 import id.ac.pnb.SnakeUp.helpers.PropertiesHelper;
 import id.ac.pnb.SnakeUp.panels.MainGame;
+import id.ac.pnb.SnakeUp.utils.GlobalVars;
 
 public class Game implements Runnable {
 
@@ -19,32 +20,55 @@ public class Game implements Runnable {
   @Override
   public void run() {
     PropertiesHelper.load("configs/games.properties");
+
     var fps = Integer.parseInt(PropertiesHelper.get("FPS_GAME"));
+    var ups = Integer.parseInt(PropertiesHelper.get("UPS_SET"));
+
     var timePerFrame = 1_000_000_000.0 / fps;
-    var lastFrame = System.nanoTime();
-    var now = 0L;
+    var timePerUpdate = 1_000_000_000.0 / ups;
+
+    var previousTime = System.nanoTime();
+    var lastCheck = System.currentTimeMillis();
+
     var frame = 0;
-    var lastCheck = 0L;
+    var update = 0;
+
+    var deltaFrame = 0.0;
+    var deltaUpdate = 0.0;
 
     while (true) {
-      now = System.nanoTime();
-      if (now - lastFrame >= timePerFrame) {
+      var currentTime = System.nanoTime();
+
+      deltaFrame += (currentTime - previousTime) / timePerFrame;
+      deltaUpdate += (currentTime - previousTime) / timePerUpdate;
+
+      previousTime = currentTime;
+
+      if (deltaUpdate >= 1.0) {
+        panel.updateGame();
+        update++;
+        deltaUpdate--;
+      }
+
+      if (deltaFrame >= 1.0) {
         panel.repaint();
-        lastFrame = now;
         frame++;
+        deltaFrame--;
       }
 
       if (System.currentTimeMillis() - lastCheck >= 1000) {
         lastCheck = System.currentTimeMillis();
-//        System.out.println("Frames: " + frame);
+        System.out.println("Frames: " + frame + " | UPS: " + update);
         frame = 0;
+        update = 0;
       }
     }
   }
 
   private void _initialize() {
+    GlobalVars.playerCount = 2;
     this.thread = new Thread(this);
-    this.panel = new MainGame(2);
+    this.panel = new MainGame();
     this.window = new GameWindow(this.panel);
   }
 
